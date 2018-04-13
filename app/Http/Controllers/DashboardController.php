97f6709976194a4ae3f8c00a4a4d88f738d06a53
https://vincentbattez.fr <vincent.battez.pro@gmail.com>
2018-04-13 22:07:24 +0200

    refactor: comment code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,11 +7,32 @@ use Auth;
 
 class DashboardController extends Controller
 {
+    /*———————————————————————————————————*\
+                    INDEX
+    \*———————————————————————————————————/*
+        @logged
+            @type      [View]
+            @method    GET
+            
+            @return    Affiche le dashboard
+            @location  /resources/views/dashboard/index.blade.php
+
+        @not logged
+            @type      [View]
+            @method    GET
+            
+            @return    Affiche la page login
+            @location  /resources/views/auth/login.blade.php
+
+            @redirect  Redirige vers la route nommé login
+    */
     public function index() {
-        if(Auth::check()){
+        if(Auth::check()) {
+            // logged
             return view('dashboard/index');
-        } else{
-            return view('auth/login');
+        } else {
+            // not logged
+            return redirect(301)->route('login');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,31 @@
 <?php
+/*———————————————————————————————————*\
+    $ LANDING PAGE
+\*———————————————————————————————————*/
+Route::get('/','LandingPageController@index')
+        ->name('landingPage.index')
+      ;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/*———————————————————————————————————*\
+    $ DASHBOARD
+\*———————————————————————————————————*/
+Route::get('/dashboard','DashboardController@index')
+        ->middleware('auth')
+        ->name('dashboard.index')
+      ;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+/*———————————————————————————————————*\
+    $ PROJECT
+\*———————————————————————————————————*/
+Route::get('/project/{id}','ProjectController@index')
+        ->where('id', '[0-9]+')
+        ->middleware('auth')
+      ;
 
-
-//Route::resource('/','Dashboard');
-
-
-Route::get('/','LandingPageController@index');
-
-
-Route::get('/dashboard','DashboardController@index')->middleware('auth');
-Route::get('/project/{id}','ProjectController@index')->where('id', '[0-9]+')->middleware('auth');
-
-
+/*———————————————————————————————————*\
+    $ AUTH
+\*———————————————————————————————————*/
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')
+        ->name('logout')
+      ;
